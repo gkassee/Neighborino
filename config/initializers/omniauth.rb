f7cdbb7f2c96c540f39b44e696f4c771441e9d85
Greg Kassee <gkassee@gmail.com>
2016-06-23 19:10:41 -0400

    Adding the ability to tweet to users who live within 5 miles of the area where a question has been posted.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-	provider :twitter, 'LilF5kwTPfFpIlACWbPw1bSJ4', 'xWopeNjmhxa768bXIE53RP11shZA8PAokzXCAWaNAmB9zAt01n'
+	provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
 end
