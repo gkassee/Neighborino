70a92811e8b4f5e30af0e4176ff4de489a84d8b8
Greg Kassee <gkassee@gmail.com>
2016-06-23 15:11:49 -0400

    Adding the ability to find users who are near a question post location

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,44 +8,6 @@ class QuestionsController < ApplicationController
     @user = User.all
   	@question = Question.new(question_params)
 
-
-    if @question.save
-
-    #     @user.each do |user|
-
-    #         def distance_between(lat1, lon1, lat2, lon2, options = {})
-
-    #           # set default options
-    #           options[:units] ||= :mi
-
-    #           # convert degrees to radians
-    #           lat1, lon1, lat2, lon2 = to_radians(lat1, lon1, lat2, lon2)
-
-    #           # compute deltas
-    #           dlat = lat2 - lat1
-    #           dlon = lon2 - lon1
-
-    #           a = (Math.sin(dlat / 2))**2 + Math.cos(lat1) *
-    #               (Math.sin(dlon / 2))**2 * Math.cos(lat2)
-    #           c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a))
-    #           c * earth_radius(options[:units])
-    #         end
-
-    #       @lat1 = @question.latitude
-    #       @lon1 = @question.longitude
-    #       @lat2 = user.latitude
-    #       @lon2 = user.longitude
-
-    #       puts @lat1
-    #       puts @lon1
-    #       puts @lat2
-    #       puts @lon2
-
-    #     end
-
-
-
-
   		redirect_to user_path(current_user)
   	# else
   	# 	redirect_to posts_path
@@ -60,8 +22,7 @@ class QuestionsController < ApplicationController
   def show
   	@question = Question.find(params[:id])
     @comment = Comment.new
-    # @user = User.all
-     @user = User.near("#{@question.latitude}, #{@question.longitude}", 5)
+     @user = User.near("#{@question.latitude}, #{@question.longitude}", 1)
   end
 
 
