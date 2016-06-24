8052cf1317eecd118843f1a6fb2035d7bcf38a14
Greg Kassee <gkassee@gmail.com>
2016-06-24 17:48:32 -0400

    heroku update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-Rails.application.config.middleware.use OmniAuth::Builder do 
-	# provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
-	# provider :twitter, Rails.application.secrets.CONSUMER_KEY, Rails.application.secrets.CONSUMER_SECRET
+Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :twitter, 'uyZ1Z1z4FbY9XLvxDjYVBja3g', 'WTwaZVdH5vHqjNSgssWnjtRvFFuxBYnctVEuZr9aGj5mSQcWsq'
 end
