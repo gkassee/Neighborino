5c9c430253c41851a1711c144a4ea4d246a47610
Greg Kassee <gkassee@gmail.com>
2016-06-27 01:30:03 -0400

    Fixing issue where home page would not load without an active session.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,11 @@ class HomeController < ApplicationController
 
   def index
   	@user = current_user
-  	@question = Question.near("#{@user.address}", 1)
+
+  	if current_user
+  		@question = Question.near("#{@user.address}", 1)
+  	end
+  	
   	if current_user && current_user.address1 == nil
 
   		redirect_to edit_user_path(@user)
