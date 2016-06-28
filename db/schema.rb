309a6f4221c537ae8e4f2183942333cf34f9c28b
Greg Kassee <gkassee@gmail.com>
2016-06-28 15:46:49 -0400

    Adding update to remove extra code from merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 20160626071855) do
     t.string   "body"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "address"
   end
 
